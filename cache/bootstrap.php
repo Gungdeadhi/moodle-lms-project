f1c960343293a31dabd5caf5687cd3dc7bcbc142
fadhilarrafi <fadhilarraafi@gmail.com>
2025-12-18 02:38:18 +0700

    Remove Moodle runtime/cache directories from repository

--- cache/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-// ********** This file is generated DO NOT EDIT **********
-$CFG->siteidentifier = 'gFRO6vYZmXCuoChvl1NfBQTyBpsnNGgpmoodle.test';
-$CFG->bootstraphash = '934cc2d86ff49700e9b6320edcf3e195';
-// Only if the file is not stale and has not been defined.
-if ($CFG->bootstraphash === hash_local_config_cache() && !defined('SYSCONTEXTID')) {
-    define('SYSCONTEXTID', 1);
-}
