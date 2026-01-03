63c00952cc1043fb17f9558931e2c18f318db9fa
Gungdeadhi <agungadhi54@gmail.com>
2026-01-03 23:31:26 +0700

    prepare to add image poster in dashboard

--- public/theme/jeho_template/lang/en/theme_jeho_template.php
+++ public/theme/jeho_template/lang/en/theme_jeho_template.php
@@ -8,3 +8,5 @@ $string['presetfiles'] = 'Additional theme preset files';
 $string['presetfiles_desc'] = 'Preset files can be used to dramatically alter the appearance of a theme.';
 $string['preset_desc'] = 'Pick a preset to broadly change the look of the theme.';
 $string['generalsettings'] = 'General Settings';
+$string['imageposter'] = 'Image Poster Dashboard';
+$string['imageposter_desc'] = 'Add poster image in dashboard';
